063d29efd19d554b5991435558425573183cd745
Anshu <anshuswn10@gmail.com>
2025-04-27 02:36:44 +0530

    feat: add User and Expense Mongoose models
    - Created User model with fields: name, email (unique), and password.
    - Added pre-save middleware to hash user passwords using bcrypt before saving.
    - Created Expense model with fields: amount, category, description, date (default to current date), and a reference to the User model.

---
+++ backend/models/Expense.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const expenseSchema = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Expense", expenseSchema);
